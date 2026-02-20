9e5833baca0948d97ca90dadc23eac64333a11b5
satyanarayanaraju4 <satyanarayanarajuchinta4@gmail.com>
2026-02-20 21:52:38 +0530

    Delete javanode.js

--- javanode.js
+++
@@ -1,6 +0,0 @@
-var http=require('http');
-http.createServer(function(req,res){
-  res.write("<h1>welcome to javascript with node.js</h1>");
-  res.end();
-}).listen(3000);
-console.log("server responded");
